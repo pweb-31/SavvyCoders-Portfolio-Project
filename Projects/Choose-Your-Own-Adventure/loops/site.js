5ff39595a0c59774cb96a1ee6c128a21e881f961
patrick <patrick@aeshdesign.com>
2016-11-17 20:59:12 -0600

    rock paper scissors 1st try

--- Projects/Choose-Your-Own-Adventure/loops/site.js
+++ Projects/Choose-Your-Own-Adventure/loops/site.js
@@ -1,21 +1,66 @@
 var output = document.querySelector( "#output" );
+var outputList = "<ul>";
 var counter = 100;
 
 while( counter > 0 ){
 	if( counter % 15 === 0 ){
-		console.log( "FizzBuzz" );
+		outputList += "<li>FizzBuzz</li>";
     }
 	else if( counter % 5 === 0 ){
-		console.log( "buzz" );
+		outputList += "<li>Fizz</li>";
     }
 	else if( counter % 3 === 0 ){
-		console.log( "fizz" );
+		outputList += "<li>Buzz</li>";
     }
 	else{
-		console.log( counter );
+		outputList += "<li>" + counter +"</ul>";
     }
 
 	counter--;
 }
 
-output.innerHTML = ""
+outputList += "</ul>";
+
+output.innerHTML = outputList;
+
+//rock, paper, scissors
+
+var runRound = function(){
+	var userChoice = prompt( "what would you like to play?  Rock, paper or scissors?")
+	var rng = Math.random();
+	var computerChoice = "Scissors";
+	var winner = "computer";
+
+	if( rng < 0.33 ){
+		computerChoice = "Rock";
+	}
+	else if( rng < 0.66 ){
+		computerChoice = "Paper"
+	}
+
+	if ( computerChoice === "Scissors" && userChoice === "Rock"){
+		winner = "user";
+	}
+	else if ( computerChoice === "Paper" && userChoice === "Scissors"){
+		winner = "user";
+	}
+	else if (computerChoice === "Rock" && userChoice === "Paper"){
+		winner = "user";
+	}
+
+	if ( computerChoice === userChoice ){
+			alert( "There is a tie!" );
+			runRound();
+	}
+	else{
+			alert( "the winner was " + winner );
+	}
+}
+
+var roundCounter = 1;
+
+while( roundCounter <= 5){
+	runRound();
+
+	roundCounter ++;
+}
