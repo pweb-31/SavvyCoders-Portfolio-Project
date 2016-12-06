8e6ef4a573d63dc5347a014f625838bd48f0a024
patrick <patrick@aeshdesign.com>
2016-12-05 20:58:07 -0600

    events using jquery

--- Projects/Sandbox/site.js
+++ Projects/Sandbox/site.js
@@ -2,33 +2,33 @@
 )
   var $title = $( "h1" );
   var $output = $( "#output");
-  var userColor = prompt ( "What color heading would you like?" );
+//  var userColor = prompt ( "What color heading would you like?" );
 
-  $title.animate ( {
-      "font-size": "500%"
-    }, {
-      "duration": 2000,
-      "complete": function() {
-        $title.css ({
-          "color": userColor,
-          "text-align": "right",
-          "font-weight": "bolder"
-        } )
-      }
-  } );
+//  $title.animate ( {
+//      "font-size": "500%"
+//    }, {
+//      "duration": 2000,
+//      "complete": function() {
+//        $title.css ({
+//          "color": userColor,
+//          "text-align": "right",
+//          "font-weight": "bolder"
+//        } )
+//      }
+//  } );
 
-  $title.animate ( {
-      "font-size": "200%"
-  }, {
-      "duration": 4000,
-      "complete": function() {
-        $title.css ({
-          "color": "black",
-          "text-align": "center",
-          "font-weight": "bolder"
-        } )
-      }
-  } );
+//  $title.animate ( {
+//      "font-size": "200%"
+//  }, {
+//      "duration": 4000,
+//      "complete": function() {
+//        $title.css ({
+//          "color": "black",
+//          "text-align": "center",
+//          "font-weight": "bolder"
+//        } )
+//      }
+//  } );
 
 
   $title.text ( "An even sweeter header!" );
@@ -41,10 +41,22 @@
   //$output.append ( "A text add-on!" );
   //$output.append ( "<h3>An awesome sub-heading</h3>" )
 
-  $( " #output > ol ").prepend(
-    $( "<li>" ).text( "step zero")
-  );
+//  $( " #output > ol ").prepend(
+//    $( "<li>" ).text( "step zero")
+//  );
 
-  $( " #output > ol ").append(
-    $( "<li>" ).text( "step four as an appendage" )
-  );
+//  $( " #output > ol ").append(
+//    $( "<li>" ).text( "step four as an appendage" )
+//  );
+
+  $( "#output" ).on( "mouseenter" , "li" , function (){
+    $( this ).addClass( "makeOrange" );
+  } );
+
+  $( "#output" ).on( "mouseleave", "li" , function (){
+    $( this ).removeClass( "makeOrange" );
+  } );
+
+  $( "h1" ).on( "hover" , function (){
+    $( this ).addClass( "makeOrange" );
+  } );
